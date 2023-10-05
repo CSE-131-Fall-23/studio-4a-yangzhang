a5ff5b3e1d11a32eb56109eabe9bd4de48ff179a
yhy62 <yhy62@2ggieee.dhcp.wustl.edu>
2023-10-05 12:33:49 -0500

    123

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,5 @@
 package studio4;
-
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -15,10 +15,23 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int x = in.nextInt();
+		int y = in.nextInt();
+		int z = in.nextInt();
+		boolean fill = in.nextBoolean();
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		double c = in.nextDouble();
+		double d = in.nextDouble();
+		StdDraw.setPenColor(x,y,z);
+		StdDraw.setPenRadius(0.01);
+        StdDraw.rectangle(a,b,c,d);
 	}
 }
